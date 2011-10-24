9e417778291742018750affbd6e36cd1d2ca2614
joshcutler <josh.cutler@gmail.com>
2011-10-23 22:22:20 -0400

    Got the classifier classifying

--- lib/classyfier/naive_bayes.rb
+++ lib/classyfier/naive_bayes.rb
@@ -14,7 +14,48 @@ module Classyfier
         _learn(data_hash, category)
       end
       
+      def classify(data_hash)
+        category_scores = _category_scores(data_hash)
+        max = [:none, 0]
+        category_scores.each_pair do |key, value|
+          max = [key, value] if value > max[1]
+        end
+        return max
+      end
+      
       private
+        def _category_scores(data_hash, name='', odds={})
+          data_hash.each_pair do |key, value|
+            case value
+            when Hash
+              _category_scores(value, "#{name}_#{key}", odds)
+            when String
+              value.split(" ").each do |sub_string|
+                _calculate_posterior(key, sub_string, name, odds) unless sub_string.strip.empty?
+              end
+            else
+              _calculate_posterior(key, value, name, odds)
+            end
+          end
+          
+          #Sum probabilities
+          odds.inject({}) do |memo, (key, value)|
+            memo[key] = value.inject(0) do |acc_memo, (acc_key, acc_value)|
+              #puts "acc_memo: #{acc_memo}, acc_key: #{acc_key}, acc_value: #{acc_value}"
+              acc_memo += acc_value
+            end
+            memo
+          end
+        end
+        
+        def _calculate_posterior(key, value, name, odds)
+          @attribute_counts.each_pair do |category, raw_counts|
+            cat = odds[category] ||= {}
+            keys = raw_counts["#{name}_#{key}"] || {}
+            cat["#{name}_#{key}_#{value}"] = Float(keys[value] || 0) / @data_size
+          end
+        end
+      
         def _learn(data_hash, category, name='')
           data_hash.each_pair do |key, value|
             case value
--- test/test_classyfier.rb
+++ test/test_classyfier.rb
@@ -8,12 +8,23 @@ class TestClassyfier < Test::Unit::TestCase
       @classyfier.train({:name => "derp", :extension => "dta"}, :data)
       @classyfier.train({:name => "malerp", :extension => "dta"}, :data)
       @classyfier.train({:name => "derp malerp", :extension => "pdf"}, :not_data)
+      @classyfier.train({:name => "foo bar", :extension => "dta"}, :not_data)
       @classyfier.train({:name => "hi", :extension => "dta"}, :data)
     end
     
     should "have stored the priors" do
       assert_equal 3, @classyfier.attribute_counts[:data]['_extension']['dta']
-      assert_equal(4, @classyfier.data_size)
+      assert_equal(5, @classyfier.data_size)
+    end
+    
+    context "and classifying an item" do
+      setup do
+        @scores = @classyfier.classify({:name => "derp hi ", :extension => "dta"})
+      end
+      
+      should "return the proper probability" do
+        assert_equal([:data, 1], @scores)
+      end
     end
   end
 end

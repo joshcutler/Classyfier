7b1bbc456633bc687b545c77a67c2e239e4873bc
joshcutler <josh.cutler@gmail.com>
2011-11-09 16:31:07 -0500

    Fixed calculations
    Added laplacean smoother

--- lib/classyfier/naive_bayes.rb
+++ lib/classyfier/naive_bayes.rb
@@ -4,10 +4,13 @@ module Classyfier
       attr_reader :data_size
       attr_reader :attribute_counts, :category_counts
       
-      def initialize()
+      def initialize(opts = {})
         @data_size = 0
         @attribute_counts = {}
         @category_counts = {}
+        
+        @laplacean_smoother = opts[:laplacean_smoother] || 0
+        @precision = opts[:precision] ? Float(opts[:precision]**10) : 100000.0
       end
       
       def train(data_hash, category)
@@ -35,8 +38,6 @@ module Classyfier
         def _category_scores(data_hash, name='', odds={})
           data_hash.each_pair do |key, value|
             case value
-            when Hash
-              _category_scores(value, "#{name}_#{key}", odds)
             when String
               value.split(" ").each do |sub_string|
                 _calculate_conditional(key, sub_string, name, odds) unless sub_string.strip.empty?
@@ -52,16 +53,22 @@ module Classyfier
             likelihood = 1
             odds[category].each_pair{|key, value| likelihood *= value}
             
-            prior = @category_counts[category] / Float(@data_size)
+            prior = (@category_counts[category] + @laplacean_smoother) / (Float(@data_size) + @laplacean_smoother*@category_counts.keys.size)
             
             marginal = 0
             odds.keys.each do |category2|
               likelihood2 = 1
-              odds[category2].each_pair{|key, value| likelihood2 *= value}
-              marginal +=  likelihood2 * (@category_counts[category2] / Float(@data_size))
+              odds[category2].each_pair do |key, value| 
+                likelihood2 *= value
+              end
+              marginal +=  likelihood2 * ((@category_counts[category2] + @laplacean_smoother) / (Float(@data_size) + @laplacean_smoother*@category_counts.keys.size))
             end
             
-            posteriors[category] = likelihood * prior / marginal
+            begin
+              posteriors[category] = ((likelihood * prior / marginal)*@precision).round / @precision
+            rescue
+              posteriors[category] = 0
+            end
           end
           
           return posteriors
@@ -71,15 +78,15 @@ module Classyfier
           @attribute_counts.each_pair do |category, raw_counts|
             cat = odds[category] ||= {}
             keys = raw_counts["#{name}_#{key}"] || {}
-            cat["#{name}_#{key}_#{value}"] = Float(keys[value] || 0) / @category_counts[category]
+            total_values = 0
+            keys.keys.each {|k| total_values += keys[k]}
+            cat["#{name}_#{key}_#{value}"] = Float((keys[value] || 0) + @laplacean_smoother) / (total_values + @laplacean_smoother*keys.keys.length)
           end
         end
       
         def _learn(data_hash, category, name='')
           data_hash.each_pair do |key, value|
             case value
-            when Hash
-              _learn(value, category, "#{name}_#{key}")
             when String
               value.split(" ").each do |sub_string|
                 _store_learned_attribute(key, sub_string, category, name) unless sub_string.strip.empty?
--- test/test_classyfier.rb
+++ test/test_classyfier.rb
@@ -2,9 +2,48 @@ require 'helper'
 
 class TestClassyfier < Test::Unit::TestCase
   
-  context "after training on a few items" do
+  context "after training on a few items with no smoothing" do
     setup do
       @classyfier = Classyfier::NaiveBayes::NaiveBayesClassifier.new
+      @classyfier.train({:name => "derp", :extension => "dta"}, :data)
+      @classyfier.train({:name => "malerp", :extension => "dta"}, :data)
+      @classyfier.train({:name => "hi", :extension => "dta"}, :data)
+      @classyfier.train({:name => "derp malerp", :extension => "pdf"}, :not_data)
+      @classyfier.train({:name => "foo bar", :extension => "dta"}, :not_data)
+    end
+    
+    should "have stored the priors" do
+      assert_equal 3, @classyfier.attribute_counts[:data]['_extension']['dta']
+      assert_equal(5, @classyfier.data_size)
+      assert_equal(3, @classyfier.category_counts[:data])
+      assert_equal(2, @classyfier.category_counts[:not_data])
+    end
+    
+    context "and classifying an item" do
+      setup do
+        @scores = @classyfier.classify({:name => "derp hi", :extension => "dta"})
+        @scores1 = @classyfier.classify({:name => "malerp", :extension => "dta"})
+        @scores2 = @classyfier.classify({:extension => "pdf"})
+        @scores3 = @classyfier.classify({:extension => "unknown"})
+      end
+      
+      should "return the proper probability" do
+        # P(data|"derp hi") = P("derp hi"|data)*P(data) / P("derp hi") = P("derp hi"|data)*P(data) / P("derp hi"|data)P(data) + P("derp hi|not")P(not)  = 
+        # P("derp"|data)*P("hi"|data)P(dta|data)*P(data) / P("derp"|data)P(hi"|data)P(dta|data)P(data) + P("derp"|not)P("hi"|not)P(dta|data)P(not)
+        # (1/3)(1/3)(3/3)(3/5)/((1/3)(1/3)(3/3)(3/5) + (1/4)(0/4)(1/2)(2/5))
+        assert_equal([:data, 1], @scores)
+        # P("malerp"|data)P(dta|data)P(data)/P("malerp"|data)P(dta|data)P(data) + P("malerp"|not_data)P(dta|not_data)P(not_data)
+        # (.333*1*.6)/(.333*1*.6+.25*.5*.4)
+        assert_equal([:data, 0.8], @scores1)
+        assert_equal([:not_data, 1], @scores2)
+        assert_equal([:none, 0], @scores3)
+      end
+    end
+  end
+  
+  context "after training on a few items with smoothing" do
+    setup do
+      @classyfier = Classyfier::NaiveBayes::NaiveBayesClassifier.new(:laplacean_smoother => 1)
       @classyfier.train({:name => "derp", :extension => "dta"}, :data)
       @classyfier.train({:name => "malerp", :extension => "dta"}, :data)
       @classyfier.train({:name => "derp malerp", :extension => "pdf"}, :not_data)
@@ -24,16 +63,17 @@ class TestClassyfier < Test::Unit::TestCase
         @scores = @classyfier.classify({:name => "derp hi", :extension => "dta"})
         @scores1 = @classyfier.classify({:name => "malerp", :extension => "dta"})
         @scores2 = @classyfier.classify({:extension => "pdf"})
+        @scores3 = @classyfier.classify({:extension => "unknown"})
       end
       
       should "return the proper probability" do
         # P(data|"derp hi") = P("derp hi"|data)*P(data) / P("derp hi") = P("derp hi"|data)*P(data) / P("derp hi"|data)P(data) + P("derp hi|not")P(not)  = 
-        # P("derp"|data)*P("hi"|data)*P(data) / P("derp"|data)P(hi"|data)P(data) + P("derp"|not)P("hi"|not)P(not)
-        assert_equal([:data, 1], @scores)
-        # P("malerp"|data)P(dta|data)P(data)/P("malerp"|data)P(dta|data)P(data) + P("malerp"|not_data)P(dta|not_data)P(not_data)
-        # .333*1*.6/(.333*1*.6+.5*.5*.4)
-        assert_equal([:data, 2/3.0], @scores1)
-        assert_equal([:not_data, 1], @scores2)
+        # P("derp"|data)*P("hi"|data)*P(dta|data)*P(data) / P("derp"|data)P(hi"|data)P(dta|data)P(data) + P("derp"|not)P("hi"|not)P(dta|not)P(not)
+        #((1+1)/(3+3))((1+1)/(3+3))((3+1)/(3+1))((3+1)/(5+2))/(((1+1)/(3+3))((1+1)/(3+3))((3+1)/(3+1))((3+1)/(5+2)) + ((1+1)/(4+4))((0+1)/(4+4))((1+1)/(2+2))((2+1)/(5+2)))
+        assert_equal([:data, 0.90459], @scores)
+        assert_equal([:data, 0.78049], @scores1)
+        assert_equal([:not_data, 0.6], @scores2)
+        assert_equal([:data, 0.57143], @scores3)
       end
     end
   end
